460c7ca64190d360c3b14dc8ce4f67d22a89fbc1
sunridden <kevinszeto9449@gmail.com>
2023-10-10 12:15:06 -0700

    Add ico file types in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
      {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       test: /\.(png|svg|ico|jpg|jpeg|gif)$/i,
        type: 'asset/resource',
      },
     ],
